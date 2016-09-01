6ae2f5e3c4aaf68a000b2295fcaaa75b3465b7fb
Andrea <andrea.rubiop88@hotmail.com>
2016-09-01 16:49:04 +0200

    Add files via upload

--- coverage.sh
+++ coverage.sh
@@ -19,6 +19,7 @@ refDB=$2
 #	INITIALIZE VARIABLES
 sampleName=$(basename $sampleDir) # gets the sample name
 organismDir=$(echo $sampleDir | rev | cut -d'/' -f3 | rev) # gets the 3 to last column (xx-organism)
+workingDir="$(echo $sampleDir | rev | cut -d'/' -f4- | rev)/SRC/" # gets up to the 3 to last column 
 organism="${organismDir##*-}" # gets what is after the '-' and assumes is the organism
 genomeLength="${refDB}/WG/genome_length.txt"
 genomeCov="${sampleDir}/coverage/${sampleName}_genome_coverage.txt"
@@ -26,6 +27,7 @@ sampleBam="${sampleDir}/reads/*sorted.bam"
 lablog="${sampleDir}/coverage/${sampleName}_coverage_log.log"
 
 module load bedtools2/bedtools2-2.25.0
+module load R/R-3.2.5
 
 echo -e "$(date)" 
 echo -e "*********** CALCULATING COVERAGE OF $organism IN SAMPLE $sampleName ************"
@@ -44,6 +46,12 @@ echo -e "The command is: ### bedtools genomecov -ibam $sampleBam -g $genomeLengt
 bedtools genomecov -ibam $sampleBam -g $genomeLength > $genomeCov
 echo -e "$(date)\t finished running genomecov for ${sampleName}\n" >> $lablog
 
+
+################################################################################################	
+echo -e "$(date)\t start generating coverage graphs for ${sampleName}\n" >> $lablog
+echo -e "The command is: ### Rscript --vanilla ${sampleDir}/coverage/ ${sampleName} ###" >> $lablog
+Rscript --vanilla "${workingDir}/graphs_coverage.R" "${sampleDir}/coverage/" ${sampleName}
+echo -e "$(date)\t finished coverage graphs for ${sampleName}\n" >> $lablog
 
 
 
