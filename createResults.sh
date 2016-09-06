3c14fcd5470ca7b9a80c6c63ce4645ba01c0dcf7
Andrea <andrea.rubiop88@hotmail.com>
2016-09-06 16:11:35 +0200

    Add files via upload

--- createResults.sh
+++ createResults.sh
@@ -22,64 +22,55 @@ echo "
    		 echo "$sampleName blast" >> $result_blastn_page
    		 echo "
    		</title>
-   		<link rel='stylesheet' type='text/css' href='style.css'>
+   		<link rel='stylesheet' type='text/css' href='css/table.css'>
+   		<link rel='stylesheet' type='text/css' href='css/deco.css' />
    	 	<meta content=''>
 	</head>
 	<body>
-		<table class='flatTable'>
-	   		<tr class='titleTr'>
-				<td class='titleTd'> " >> $result_blastn_page
-				echo "${sampleName} blast result" >> $result_blastn_page
-				echo "				
-				</td>
-				<td colspan='4'></td>
-				<td class='plusTd button'></td>
-			</tr>
-			<!-- <tr class='headingTr'> -->
-			<thead class='headingTr'>
-				<td>Subject Title</td>
-				<td>Query Seq-id</td>
-				<td>Reference Id</td>
-				<td>Percentage of identical matches</td>
-				<td>Alignment length</td>
-				<td>Number of mismatches</td>
-				<td>Number of gap openings</td>
-				<td>Start of alignment in query</td>
-				<td>End of alignment in query</td>
-				<td>Start of alignment in subject</td>
-				<td>End of alignment in subject</td>
-				<td>Expect value</td>
-				<td>Bit score</td>
-				<td>Genome coverage</td>
-			</thead>" >> $result_blastn_page
-			#	Start formatting data from blast
-			while IFS='' read -r line || [[ -n $line ]]
-			do
-				echo "
-			<tr>" >> $result_blastn_page
-				IFS='	' read -r -a array <<< "$line"
-				for value in "${array[@]:0:13}"
+		<table>
+			<thead>
+		   		<tr>
+		   			<th>" >> $result_blastn_page
+					echo "${sampleName} blast result" >> $result_blastn_page
+					echo "</th>
+					<!--<th>Subject Title</th>-->
+					<th>Query Seq-id</th>
+					<th>Reference Id</th>
+					<th>Percentage of identical matches</th>
+					<th>Alignment length</th>
+					<th>Number of mismatches</th>
+					<th>Number of gap openings</th>
+					<th>Start of alignment in query</th>
+					<th>End of alignment in query</th>
+					<th>Start of alignment in subject</th>
+					<th>End of alignment in subject</th>
+					<th>Expect value</th>
+					<th>Bit score</th>
+					<th>Genome coverage</th>
+				</tr>
+			</thead>
+			<tbody>" >> $result_blastn_page
+				#	Start formatting data from blast
+				while IFS='' read -r line || [[ -n $line ]]
 				do
-					echo "<td>${value}</td>" >> $result_blastn_page
-				done
-				# conseguir el genome id
-				genomeId="${array[2]}"
-				coverageFile="${sampleDir}coverage/${genomeId}_coverage_graph.pdf"
-				echo "<td><a href="$coverageFile">${genomeId}</a></td>" >> $result_blastn_page
-				echo "
-      		</tr>
-      		<!--
-				<td class='controlTd'>
-	  				<div class='settingsIcons'>
-	    				<span class='settingsIcon'><img src='http://i.imgur.com/nnzONel.png' alt='X' /></span>
-	    				<span class='settingsIcon'><img src='http://i.imgur.com/UAdSFIg.png' alt='placeholder icon' /></span>
-	    				<div class='settingsIcon'><img src='http://i.imgur.com/UAdSFIg.png' alt='placeholder icon' /></div>
-	  				</div>  
-				</td>
-			-->
-      		</tr>" >> $result_blastn_page
-		done < ${blastnResult}
-			echo "
+					echo "
+				<tr>" >> $result_blastn_page
+					IFS='	' read -r -a array <<< "$line"
+					echo "<th>${array[0]}</th>">> $result_blastn_page
+					for value in "${array[@]:1:12}"
+					do
+						echo "<td>${value}</td>" >> $result_blastn_page
+					done
+					# conseguir el genome id
+					genomeId="${array[2]}"
+					coverageFile="${sampleDir}coverage/${genomeId}_coverage_graph.pdf"
+					echo "<td><a href="$coverageFile">${genomeId}</a></td>" >> $result_blastn_page
+					echo "</tr>" >> $result_blastn_page
+			done < ${blastnResult}
+		echo "</tbody>
 		</table>
+		<script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
+		<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery-throttle-debounce/1.1/jquery.ba-throttle-debounce.min.js'></script>
+		<script src='js/jquery.stickyheader.js'></script>
 	</body>
 </html>" >> $result_blastn_page
