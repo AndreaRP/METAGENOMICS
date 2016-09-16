027fdddd3e0ba8417975b4e39f2ac77da0909d59
Andrea <andrea.rubiop88@hotmail.com>
2016-09-16 14:18:58 +0200

    Add files via upload

--- html/js/functionality.js
+++ html/js/functionality.js
@@ -1,3 +1,14 @@
 function load_element(element){
-document.getElementById(element).innerHTML='<object type="text/html" data="blast.html"></object>';
+	document.getElementById('pagina').innerHTML='<object class ="full" type="text/html" data="'+element+'.html"></object>';
 } 
+
+$(document).ready(function(){
+   $(".menu").click(function(){
+       	$('a.selected').removeClass('selected');
+        $(this).addClass('selected');        
+		}); 
+   $(".tabs-style-bar nav ul li").click(function(){
+       	$('.tab-current').removeClass('tab-current');
+        $(this).addClass('tab-current');        
+		}); 
+});
