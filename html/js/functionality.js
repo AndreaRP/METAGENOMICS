604bd6fd87d53246cf1f92a0635bb2391cf0b6fd
Andrea <andrea.rubiop88@hotmail.com>
2016-09-23 14:36:26 +0200

    Add files via upload

--- html/js/functionality.js
+++ html/js/functionality.js
@@ -1,8 +1,15 @@
+var margin;
+
 function load_element(element){
-	document.getElementById('pagina').innerHTML='<object class ="full" type="text/html" data="'+element+'.html"></object>';
+	get_size();
+	document.getElementById('pagina').innerHTML='<div hidden="true" id="prueba">'+margin+'</div><object type="text/html" data="'+element+'.html"></object>';
+	
+	/*document.getElementById('pagina').innerHTML='<iframe src="'+element+'.html"></iframe>';*/
+	/*set_size();*/
 } 
 
 $(document).ready(function(){
+	/*changes properties of naviagtion menus*/
    $(".menu").click(function(){
        	$('a.selected').removeClass('selected');
         $(this).addClass('selected');        
@@ -11,4 +18,27 @@ $(document).ready(function(){
        	$('.tab-current').removeClass('tab-current');
         $(this).addClass('tab-current');        
 		}); 
+
+   /*$("#horizontal-nav").on("load", function(){
+   		margin = $('#horizontal-nav').css('marginLeft').replace(/[^-\d\.]/g, '');
+   		alert('margin1: '+ margin);
+   		});
+
+   /*$(window).resize(function(){
+   		margin = $('#horizontal-nav').css('marginLeft');
+   		alert('margin: '+margin);
+		$('#vertical-bar').css("width",margin);
+		alert('width: '+$('#vertical-bar').css('width'));
+   });*/
+   	/*set_size();*/
 });
+
+function get_size(){
+	margin = $('#horizontal-nav').css('marginLeft').replace(/[^-\d\.]/g, '');
+}
+
+function set_size(){
+	/*margin = $('#horizontal-nav').css('marginLeft');*/
+	alert('margin2: '+ $('#prueba').html());
+	$('#vertical-bar').css("width",$('#prueba').text());
+}
