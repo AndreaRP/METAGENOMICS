3a002fe32e14f6e35d13d0d9d31cc7e138e376e9
Andrea <andrea.rubiop88@hotmail.com>
2016-11-02 17:54:17 +0100

    Add files via upload

---
+++ html/js/main-nav.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+    /*changes properties of naviagtion menus*/
+    /*set_size();*/
+    $(".menu").click(function() {
+        // Change style of the selected sample
+        $('a.selected').removeClass('selected');
+        $(this).addClass('selected');
+    });
+    $(".tabs-style-bar nav ul li").click(function() {
+        $('.tab-current').removeClass('tab-current');
+        $(this).addClass('tab-current');
+    });
+});
